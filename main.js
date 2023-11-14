2e2611f3a55acccaa3a0830d600373bad46ea118
ali <ojdwq@gmail.com>
2023-11-14 21:51:01 +0500

    birinchi commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var names = prompt('Ismingizni kiriting')
+if (names === 'jovlon') {
+    console.log('Xush kelibsiz');
+} else{
+    console.log('Sizni tanimadik!');
+}
+var userYears = prompt('Yoshingizni kiriting!')
+if (userYears === 1-18) {
+    console.log('xali yoshsiz');
+} if (userYears === 18-40) {
+    console.log('ishlasingiz kerak');
+} else{
+    console.log('nimadur xato');
+}
